1f270d7890e7b2697b4fae08aa159685d7dd758f
Michael Kennedy <merpso@gmail.com>
2020-10-26 17:15:00 +1100

    some additional clean-up for cloud-init

--- providers.tf
+++ providers.tf
@@ -1,15 +1,3 @@
 terraform {
-  required_providers {
-    bigip = {
-      source = "F5Networks/bigip"
-      version = "1.4"
-    }
-    aws = {
-      source = "hashicorp/aws"
-    }
-    random = {
-      source = "hashicorp/random"
-    }
-  }
   required_version = ">= 0.13"
 }
